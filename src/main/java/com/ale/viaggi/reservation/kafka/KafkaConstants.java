30e7806b5ebd1e6bbd59986e81abe5a1d968ae47
Alessio <alessio.bottaini@gmail.com>
2020-12-26 16:34:38 +0100

    update

--- src/main/java/com/ale/viaggi/reservation/kafka/KafkaConstants.java
+++
@@ -1,9 +0,0 @@
-// package com.ale.viaggi.reservation.kafka;
-//
-// public final class KafkaConstants {
-// public static final String KAFKA_SERVERS = "192.168.2.114:9092";
-// public static final String SCHEMA_REGISTRY_URL = "http://192.168.2.114:8081";
-// public static final String KEY_PARTITION_RESERVATION =
-// "reservationPartition";
-//
-// }
